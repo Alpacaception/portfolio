7152c6e2f5594f09933c66c6f9541c26848a224a
Alpacaception <applebyr@outlook.com>
2022-06-08 00:18:27 +1000

    basic abt me

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,47 @@
 <template>
   <main class="bg-richBlack min-h-screen">
+    <!--Header Div-->
     <div>
-      <div class="flex justify-left pl-8">
+      <div class="flex justify-left pl-8 fixed">
         <img src="./assets/icon.svg" width="60" height="60" class="">
         <h1 class="text-2xl font-mono p-6 text-white font-ubuntu font-bold transition duration-300 hover:text-green">
           Rhys Appleby
         </h1>
       </div>
     </div>
+    <!--Landing page Div-->
     <div class="flex justify-left">
+      <!--LHS nav Div-->
       <div class="flex justify-left flex-col justify-center min-h-screen pb-28 pl-4 fixed">
         <img src="./assets/line-l.svg" class="">
         <img src="./assets/line-s.svg" class="">
         <img src="./assets/line-s.svg" class="">
         <img src="./assets/line-s.svg" class="">
       </div>
+      <!--Intro Div-->
       <div class="">
-        <h1 class="text-8xl font-mono px-72 py-40 text-white font-ubuntu font-bold transition duration-300 hover:text-green">
+        <h1 class="text-8xl px-72 py-40 text-white font-ubuntu font-bold transition duration-300 hover:text-green">
           Rhys Appleby
         </h1>
+        <!--<a href="#" v-scroll-to="'#about'">click</a>-->
+      </div>
+    </div>
+    <!--About Me Div-->
+    <div class="py-[664px] px-36">
+      <div class="float-left w-1/2 ">
+        <h1 class="text-8xl text-white font-ubuntu font-bold transition duration-300 hover:text-green" id="about">
+          About Me
+        </h1>
+        <p class="text-2xl text-white font-ubuntu transition duration-300 py-12 hover:text-green">
+          Hello, I am a full stack developer from Australia. I am primarily a front-end developer, however I have a significant amount of experience on the backend with python. If you are intererested in the technologies I use you can see them below!
+        </p>
+        <!--Technology Icons-->
+        <div class="flex justify-left">
+          <img src="" class="">
+        </div>
+      </div>
+      <div class="float-right w-1/2">
+        <img src="">
       </div>
     </div>
   </main>
